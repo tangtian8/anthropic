4aed03f102b2aa6bfaf7982548306bc76b9a1584
Josh Long <54473+joshlong@users.noreply.github.com>
2025-05-16 22:09:37 -0700

    an e2e using PostgreML and Anthropic's Claude AI model

--- adoptions/db/init.sh
+++ adoptions/db/init.sh
@@ -2,6 +2,6 @@
 
 # PGPASSWORD=mypassword psql -U myappuser -h localhost -p 5433 postgresml
 
-cat users.sql |  psql -U myappuser -h localhost -p 5433 postgresml
+cat users.sql |  psql -U postgresml  -h localhost -p 5433 postgresml
 
 
